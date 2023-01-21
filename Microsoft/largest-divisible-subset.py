bbbd9d9fde7683dcb6d384acd8a23f721dcce441
harshitaneelisetty <90179226+harshitaneelisetty@users.noreply.github.com>
2023-01-21 18:15:59 +0530

    Create largest-divisible-subset.py

---
+++ Microsoft/largest-divisible-subset.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/largest-divisible-subset/
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums.sort()
+        res = [[num] for num in nums]
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(res[i]) < len(res[j]) + 1:
+                    res[i] = res[j] + [nums[i]]
+        return max(res, key = len)
