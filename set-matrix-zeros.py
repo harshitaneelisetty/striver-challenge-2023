6995f712c719d7a85724ce08d17f9e4880a6c8ea
harshitaneelisetty <90179226+harshitaneelisetty@users.noreply.github.com>
2023-06-07 16:52:41 +0530

    Create set-matrix-zeros.py

---
+++ set-matrix-zeros.py
@@ -0,0 +1,22 @@
+#https://www.codingninjas.com/codestudio/problems/set-matrix-zeros_8230862?challengeSlug=striver-sde-challenge
+
+
+def setZeros(matrix: List[List[int]]) -> None:
+    col = True
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i in range(rows):
+        if matrix[i][0] == 0:
+            col = False
+        for j in range(1, cols):
+            if matrix[i][j] == 0:
+                matrix[i][0] = matrix[0][j] = 0
+                    
+    for i in range(rows - 1, -1, -1):
+        for j in range(cols - 1, 0, -1):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+        if col == False:
+            matrix[i][0] = 0
+        
+#I iterated through the matrix twice, using the first row and first column as markers, to identify and set all rows and columns containing zeros to zeros.     
