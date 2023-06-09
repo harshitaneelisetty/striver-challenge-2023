a929a45b86aed99575a56c3dfee620389afedaf3
harshitaneelisetty <90179226+harshitaneelisetty@users.noreply.github.com>
2023-06-09 09:50:29 +0530

    Create Zalgo.py

---
+++ Zalgo.py
@@ -0,0 +1,6 @@
+#https://www.codingninjas.com/codestudio/problems/z-algorithm_8230818?challengeSlug=striver-sde-challenge
+
+def zAlgorithm(s, p, n, m):
+    return s.count(p)
+  
+#i simply counted how many times substring p occured in s using count method
